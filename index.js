87a56d08d42c7e9dfe8c67392f2d0a6f63437e13
OhmLibre <contact@ohmlibre.fr>
2020-03-13 12:42:06 +0100

    :sparkles: Adding a code base

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 require("dotenv").config();
 
 const Discord = require("discord.js");
+const fs = require("fs");
+
 const client = new Discord.Client();
 
-client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+client.settings = require("./settings");
+
+// Gestion des évènements
+fs.readdir("./events/", (err, files) => {
+	if (err) return console.error(err);
+
+	files.forEach((file) => {
+		let eventFile = require(`./events/${file}`);
+
+		client.on(file.split(".")[0], (...args) => eventFile.run(client, ...args));
+	});
 });
 
-client.on("message", msg => {
-	if (msg.content === "ping") {
-		msg.reply("Pong!");
-	}
+// Gestion des commandes
+client.on("message", (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(client.settings.prefix)) return;
+
+	let command = message.content.split(" ")[0].slice(client.settings.prefix.length);
+	let args = message.content.split(" ").slice(1);
+
+	fs.access(`./commands/${command}.js`, fs.F_OK, (err) => {
+		if (err) return;
+
+		let commandFile = require(`./commands/${command}.js`);
+
+		commandFile.run(client, message, args);
+	});
 });
 
 client.login(process.env.BOT_TOKEN);
